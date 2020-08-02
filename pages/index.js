5dc68e04d3a638ab1b2b836d29bf5908853be76a
vinay <vinay@eastbidwell.com>
2020-08-02 00:17:47 -0400

    Added some tyling components to sidebar and main content section

--- pages/index.js
+++ pages/index.js
@@ -17,133 +17,130 @@ export default class Home extends Component {
   
   state = { visible: false, setVisible: false };
   handleItemClick = () => this.setState({ visible: !this.state.visible })
- render(){
-  return (
-    <div className="container">
-      <Head>
-        <title>East Bidwell</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-          
-     <Menu inverted attached='top'>
-        <Menu.Item 
-        onClick={this.handleItemClick} 
-        >
-         MENU >
-        </Menu.Item>
-
-        <Menu.Menu position='right'>
-          <Menu.Item
-            name='signup'
-          >
-            Sign Up
-          </Menu.Item>
-
-          <Menu.Item
-            name='help'
-          >
-            Help
-          </Menu.Item>
-        </Menu.Menu>
-      </Menu>
-          
-    
-    
-
-
-    
-    
-    
-          <Sidebar
-            as={Menu}
-            animation='overlay'
-            icon='labeled'
-            inverted
-            
-            vertical
-            visible={this.state.visible}
-            width='thin'
-          >
-                 <Menu.Item 
-                    onClick={this.handleItemClick} 
-                    >
-                      Close
-                    </Menu.Item>
-            <Menu.Item as='a'>
-              <Icon name='home' />
-              Home
-            </Menu.Item>
-            <Menu.Item as='a'>
-              <Icon name='gamepad' />
-              Games
-            </Menu.Item>
-            <Menu.Item as='a'>
-              <Icon name='camera' />
-              Channels
-            </Menu.Item>
-          </Sidebar>
-
-         
-            
  
 
+   render(){
+      return (
+        
+          <div className="container">
+        
+            <Head>
+              <title>East Bidwell</title>
+              <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-       <Checkbox
-          checked={this.state.visible}
-          label={{ children: <code>visible</code> }}
-          onChange={this.handleItemClick}
-        />
+           <Menu inverted attached='top' size='small'>
+              <Menu.Item 
+              onClick={this.handleItemClick} 
+              >
+               MENU >
+              </Menu.Item>
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+              <Menu.Menu position='right'>
+                <Menu.Item
+                >
+                  EAST BIDWELL
+                </Menu.Item>
+              </Menu.Menu>
+            </Menu>
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        
+            <Sidebar
+              as={Menu}
+              animation='overlay'
+              icon='labeled'
+              inverted
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+              vertical
+              visible={this.state.visible}
+              width='wide'
+            >
+                   <Menu.Item 
+                      onClick={this.handleItemClick} 
+                      >
+                        Close
+                      </Menu.Item>
+              <div className= "divcenter">
+                <Menu.Item as='a'>
+                  Call, Text or Email us anytime!
+                </Menu.Item>
+                <Menu.Item as='a'>
+                  <Icon name='phone' />
+                  305-814-7210
+                </Menu.Item>
+                <Menu.Item as='a'>
+                  <Icon name='mail' />
+                  hello@eastbidwell.com
+                </Menu.Item>
+              </div>
+            </Sidebar>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+ <div className="contentone">
+ </div>
+ <div className="contenttwo">
+ </div>
+ <div className="contentthree">
+ </div>
+        
+         
+  
+            <main>
+              <h1 className="title">
+                Welcome to <a href="https://nextjs.org">Next.js!</a>
+              </h1>
+             <Checkbox
+                checked={this.state.visible}
+                label={{ children: <code>visible</code> }}
+                onChange={this.handleItemClick}
+              />
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+              <p className="description">
+                Get started by editing <code>pages/index.js</code>
+              </p>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
+              <div className="grid">
+                <a href="https://nextjs.org/docs" className="card">
+                  <h3>Documentation &rarr;</h3>
+                  <p>Find in-depth information about Next.js features and API.</p>
+                </a>
 
+                <a href="https://nextjs.org/learn" className="card">
+                  <h3>Learn &rarr;</h3>
+                  <p>Learn about Next.js in an interactive course with quizzes!</p>
+                </a>
+
+                <a
+                  href="https://github.com/vercel/next.js/tree/master/examples"
+                  className="card"
+                >
+                  <h3>Examples &rarr;</h3>
+                  <p>Discover and deploy boilerplate example Next.js projects.</p>
+                </a>
+
+                <a
+                  href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                  className="card"
+                >
+                  <h3>Deploy &rarr;</h3>
+                  <p>
+                    Instantly deploy your Next.js site to a public URL with Vercel.
+                  </p>
+                </a>
+              </div>
+            </main>
+
+            <footer>
+              <a
+                href="https://www.eastbidwell.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Powered by East Bidwell LLC
+              </a>
+            </footer>
+
+        
+        
       <style jsx>{`
         .container {
           min-height: 100vh;
@@ -172,10 +169,31 @@ export default class Home extends Component {
           justify-content: center;
           align-items: center;
         }
-
-        footer img {
-          margin-left: 0.5rem;
+    
+       
+        .divcenter {
+         
+          padding: 28vh 0 10vh 0;
+          height: 80vh;
         }
+        
+
+        .contentone {
+          background-color: #fff;
+          width: 100%;
+          height: 100vh;
+        }
+        .contenttwo {
+          background-color: #eaeaea;
+          width: 100%;
+          height: 100vh;
+        }
+        .contentthree {
+          background-color: #fff;
+          width: 100%;
+          height: 100vh;
+        }
+
 
         footer a {
           display: flex;
